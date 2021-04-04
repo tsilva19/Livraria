9603cf99042af9b14d6c874aeab6d99df78b6372
Thiago <thiagoalves060@gmail.com>
2021-04-04 11:24:54 -0300

    Primeiro Commit

---
+++ READEME.md
@@ -0,0 +1,3 @@
+## Projeto em Construção
+
+Linguagem JAVA 
---
+++ src/main/java/CalculadoraDeEstoque.java
@@ -0,0 +1,18 @@
+public class CalculadoraDeEstoque {
+    public static  void  main(String[] args){
+      double  livroJava8 = 59.90;
+      double  livroTDD = 59.90;
+
+      double soma = livroTDD + livroJava8;
+
+      System.out.println("O total em estoque é " + soma);
+
+      if(soma < 150) {
+          System.out.println("Seu estoque está muito baixo!");
+      } else if (soma >= 2000){
+           System.out.println("Seu estoque está muito alto");
+        }else {
+          System.out.println(" Seu estoque está bom ");
+      }
+    }
+}
